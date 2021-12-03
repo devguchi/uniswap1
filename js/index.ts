ce299f31431c66daddea93170bc40438a5fbbb23
devguchi <devguchitaro@gmail.com>
2021-12-03 12:22:37 +0900

    added JsonRpcProvider

--- js/index.ts
+++ js/index.ts
@@ -1,1 +1,6 @@
-console.log('hoge');
+import {ethers} from "ethers";
+import {Address} from "cluster";
+require('dotenv').config();
+
+const endpoint = process.env.ENDPOINT;
+const provider = new ethers.providers.JsonRpcProvider(endpoint);
